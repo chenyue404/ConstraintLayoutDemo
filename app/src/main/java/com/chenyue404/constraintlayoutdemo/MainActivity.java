bd7cbca5f3b074fa811147fa7c69653f4df711bb
chenyue404 <601450225@qq.com>
2019-06-20 09:26:36 +0800

    clock

--- app/src/main/java/com/chenyue404/constraintlayoutdemo/MainActivity.java
+++ app/src/main/java/com/chenyue404/constraintlayoutdemo/MainActivity.java
@@ -1,13 +1,29 @@
 package com.chenyue404.constraintlayoutdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "DEMO";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e(TAG, "onCreate: " + getScreenWidth());
+
     }
+
+
+    public int getScreenWidth() {
+        Resources resources = this.getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        return dm.widthPixels;
+    }
+
 }
